8a43cf7b624409e525edd991d279255e3b5e94d3
mingyuan <cnwhmmy@outlook.com>
2024-04-25 19:49:27 +0800

    1

--- cuda-code/17-cpasync/cpasynckernel2d.cu
+++ cuda-code/17-cpasync/cpasynckernel2d.cu
@@ -1,37 +1,76 @@
-#include <cuda.h>         // CUtensormap
+#define SMEM_WIDTH 64
+#define SMEM_HEIGHT 64
+
+int main()
+{
+  return 0;
+}
+
+#include <cuda.h> // CUtensormap
 #include <cuda/barrier>
 using barrier = cuda::barrier<cuda::thread_scope_block>;
 namespace cde = cuda::device::experimental;
 
-#define SMEM_WIDTH 64
-#define SMEM_HEIGHT 64
-
-__global__ void kernel(const __grid_constant__ CUtensorMap tensor_map, int x, int y) {
+__global__ void kernel(const __grid_constant__ CUtensorMap tensor_map, int x, int y)
+{
   // The destination shared memory buffer of a bulk tensor operation should be
   // 128 byte aligned.
   __shared__ alignas(128) int smem_buffer[SMEM_HEIGHT][SMEM_WIDTH];
 
-  // Initialize shared memory barrier with the number of threads participating in the barrier.
-
+// Initialize shared memory barrier with the number of threads participating in the barrier.
+#pragma nv_diag_suppress static_var_with_dynamic_init
   __shared__ barrier bar;
 
+  if (threadIdx.x == 0)
+  {
+    // Initialize barrier. All `blockDim.x` threads in block participate.
+    init(&bar, blockDim.x);
+    // Make initialized barrier visible in async proxy.
+    cde::fence_proxy_async_shared_cta();
+  }
+  // Syncthreads so initialized barrier is visible to all threads.
+  __syncthreads();
 
-
-
-  if (threadIdx.x == 0) {
+  barrier::arrival_token token;
+  if (threadIdx.x == 0)
+  {
     // Initiate bulk tensor copy.
     cde::cp_async_bulk_tensor_2d_global_to_shared(&smem_buffer, &tensor_map, x, y, bar);
     // Arrive on the barrier and tell how many bytes are expected to come in.
-
-  } else {
+    token = cuda::device::barrier_arrive_tx(bar, 1, sizeof(smem_buffer));
+  }
+  else
+  {
     // Other threads just arrive.
+    token = bar.arrive();
+  }
+  // Wait for the data to have arrived.
+  bar.wait(std::move(token));
 
+  // Symbolically modify a value in shared memory.
+  smem_buffer[0][threadIdx.x] += threadIdx.x;
+
+  // Wait for shared memory writes to be visible to TMA engine.
+  cde::fence_proxy_async_shared_cta();
+  __syncthreads();
+  // After syncthreads, writes by all threads are visible to TMA engine.
+
+  // Initiate TMA transfer to copy shared memory to global memory
+  if (threadIdx.x == 0)
+  {
+    cde::cp_async_bulk_tensor_2d_shared_to_global(&tensor_map, x, y, &smem_buffer);
+    // Wait for TMA transfer to have finished reading shared memory.
+    // Create a "bulk async-group" out of the previous bulk copy operation.
+    cde::cp_async_bulk_commit_group();
+    // Wait for the group to have completed reading from shared memory.
+    cde::cp_async_bulk_wait_group_read<0>();
   }
 
-}
-
-int main()
-{
-  
-  return 0;
+  // Destroy barrier. This invalidates the memory region of the barrier. If
+  // further computations were to take place in the kernel, this allows the
+  // memory location of the shared memory barrier to be reused.
+  if (threadIdx.x == 0)
+  {
+    (&bar)->~barrier();
+  }
 }
